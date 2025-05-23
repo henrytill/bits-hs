91de2d358b6e0471d6557f04b0c48871172a44ef
Henry Till <henrytill@gmail.com>
2025-05-22 23:02:44 -0700

    Zipper: rework

--- src/Bits/Zipper.hs
+++ src/Bits/Zipper.hs
@@ -1,5 +1,5 @@
 -- |
--- Module      : Bits.Zipper
+-- Module : Bits.Zipper
 -- Description : Huet's Zipper
 --
 -- <http://gallium.inria.fr/~huet/PUBLIC/zip.pdf Functional Pearls: The Zipper>, by Gerard Huet
@@ -49,45 +49,41 @@ data Path a
 data Location a = Loc (Tree a) (Path a) deriving (Eq, Show)
 
 -- |
--- >>> goRight $ goDown example
+-- >>> goRight . goDown $ example
 -- Loc (Item "+") (Node [Section [Item "a",Item "*",Item "b"]] Top [Section [Item "c",Item "*",Item "d"]])
--- >>> goRight $ goRight $ goDown example
+-- >>> goRight . goRight . goDown $ example
 -- Loc (Section [Item "c",Item "*",Item "d"]) (Node [Item "+",Section [Item "a",Item "*",Item "b"]] Top [])
 goRight :: Location a -> Location a
-goRight (Loc t p) = case p of
-  Top -> error "right of top"
-  Node left up (r : right) -> Loc r (Node (t : left) up right)
-  _ -> error "right of last"
+goRight (Loc _ Top) = error "right of top"
+goRight (Loc t (Node left up (r : right))) = Loc r (Node (t : left) up right)
+goRight (Loc _ (Node _ _ [])) = error "right of last"
 
 -- |
--- >>> goLeft (goRight (goDown example)) == goDown example
+-- >>> (goLeft . goRight . goDown $ example) == goDown example
 -- True
 goLeft :: Location a -> Location a
-goLeft (Loc t p) = case p of
-  Top -> error "left of top"
-  Node (l : left) up right -> Loc l (Node left up (t : right))
-  Node [] _ _ -> error "left of first"
+goLeft (Loc _ Top) = error "left of top"
+goLeft (Loc t (Node (l : left) up right)) = Loc l (Node left up (t : right))
+goLeft (Loc _ (Node [] _ _)) = error "left of first"
 
 -- |
 -- >>> goDown example
 -- Loc (Section [Item "a",Item "*",Item "b"]) (Node [] Top [Item "+",Section [Item "c",Item "*",Item "d"]])
--- >>> goDown $ goDown example
+-- >>> goDown . goDown $ example
 -- Loc (Item "a") (Node [] (Node [] Top [Item "+",Section [Item "c",Item "*",Item "d"]]) [Item "*",Item "b"])
--- >>> goRight $ goDown $ goDown example
+-- >>> goRight . goDown . goDown $ example
 -- Loc (Item "*") (Node [Item "a"] (Node [] Top [Item "+",Section [Item "c",Item "*",Item "d"]]) [Item "b"])
 goDown :: Location a -> Location a
-goDown (Loc t p) = case t of
-  Item _ -> error "down of item"
-  Section (t1 : trees) -> Loc t1 (Node [] p trees)
-  _ -> error "down of empty"
+goDown (Loc (Item _) _) = error "down of item"
+goDown (Loc (Section (t1 : trees)) p) = Loc t1 (Node [] p trees)
+goDown (Loc (Section []) _) = error "down of empty"
 
 -- |
--- >>> goUp (goDown example) == example
+-- >>> (goUp . goDown $ example) == example
 -- True
 goUp :: Location a -> Location a
-goUp (Loc t p) = case p of
-  Top -> error "up of top"
-  Node left up right -> Loc (Section (reverse left ++ (t : right))) up
+goUp (Loc _ Top) = error "up of top"
+goUp (Loc t (Node left up right)) = Loc (Section (reverse left ++ (t : right))) up
 
 -- | A function to access the /n/th son of the current tree
 --
@@ -96,8 +92,6 @@ goUp (Loc t p) = case p of
 -- >>> nthLoc example 3
 -- Loc (Section [Item "c",Item "*",Item "d"]) (Node [Item "+",Section [Item "a",Item "*",Item "b"]] Top [])
 nthLoc :: (Ord t, Num t) => Location a -> t -> Location a
-nthLoc loc = nthRec
-  where
-    nthRec 1 = goDown loc
-    nthRec n | n > 0 = goRight $ nthRec (n - 1)
-    nthRec _ = error "nthLoc expects a positive integer"
+nthLoc loc 1 = goDown loc
+nthLoc loc n | n > 0 = goRight . nthLoc loc $ n - 1
+nthLoc _ _ = error "nthLoc expects a positive integer"
